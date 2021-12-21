aaa9171210249cfbdabcf2010fa03f190ac35ba4
cxldxice <hertearsonmyneck@gmail.com>
2021-12-21 16:28:55 +0300

    1.1

--- app.py
+++ app.py
@@ -7,6 +7,7 @@
 import os
 import sys
 import zipfile
+import threading
 import glob
 import time
 
@@ -15,7 +16,8 @@ import keyboard
 import cursor
 
 
-__version__ = 1.0
+__version__ = 1.1
+__author__  = "yi6dev"
 
 #define system clear command
 if "darwin" in sys.platform or "linux" in sys.platform: clear = "clear"
@@ -35,28 +37,47 @@ except:
 class App(object):
     def __init__(self, args) -> None:
         self.colorized = False
+        self.autoload = self.__read_configs()[2]
 
         if not "--no-ascii" in args:
             colorama.init()
 
-            self.colorized = True 
+            self.colorized = True
+        
+        if "--version" in args:
+            print(f"version  = v{__version__}")
+            print(f"author   = {__author__}")
+            print(f"path     = {config_path}")
+            return
 
         #init all
         self.keyhook()
         self.configs()
         self.render()
+        self.autoloadhook()
 
-        try: 
-            time.sleep(1e6)
-        except KeyboardInterrupt:
-            os.system(clear)
 
     def keyhook(self):
         keyboard.add_hotkey("ctrl+alt+o", callback=self.__set_osu_path)
         keyboard.add_hotkey("ctrl+alt+d", callback=self.__set_download_path)
+        keyboard.add_hotkey("ctrl+alt+l", callback=self.__set_autoload)
         keyboard.add_hotkey("ctrl+alt+i", callback=self.__install)
         keyboard.add_hotkey("ctrl+alt+c", callback=self.__config)
         keyboard.add_hotkey("ctrl+alt+e", callback=self.__exit)
+
+    def autoloadhook(self):
+        while 1:
+            if self.autoload == "true":
+                beatmaps = glob.glob(f"{self.__read_configs()[1]}\*.osz")
+
+                if len(beatmaps):
+                    self.__install()
+
+            try: 
+                time.sleep(1)
+            except KeyboardInterrupt:
+                os.system(clear)
+                return
 
     def configs(self):
         if not os.path.exists(f"{config_path}\.osupath"):
@@ -69,6 +90,11 @@ class App(object):
             download.write("none")
             download.close()
 
+        if not os.path.exists(f"{config_path}\.autoload"):
+            autoload = open(f"{config_path}\.autoload", "w")
+            autoload.write("false")
+            autoload.close()
+
     def render(self):
         os.system(clear)
 
@@ -77,7 +103,13 @@ class App(object):
             print(f"{colorama.Fore.LIGHTBLACK_EX}by @yi6dev\n")
             print(f"{colorama.Fore.YELLOW}Options")
             print(f"{colorama.Fore.LIGHTBLACK_EX} - Set osu path                        {colorama.Fore.LIGHTBLUE_EX}CTRL + ALT + O")
-            print(f"{colorama.Fore.LIGHTBLACK_EX} - Set download path                   {colorama.Fore.LIGHTBLUE_EX}CTRL + ALT + D")
+            print(f"{colorama.Fore.LIGHTBLACK_EX} - Set download path                   {colorama.Fore.LIGHTBLUE_EX}CTRL + ALT + D")\
+
+            if self.autoload == "false":
+                print(f"{colorama.Fore.LIGHTBLACK_EX} - Autoload beatmaps                   {colorama.Fore.LIGHTBLUE_EX}CTRL + ALT + L")
+            else:
+                print(f"{colorama.Fore.LIGHTBLACK_EX} - {colorama.Fore.LIGHTGREEN_EX}Autoload beatmaps                   {colorama.Fore.LIGHTBLUE_EX}CTRL + ALT + L")
+
             print(f"{colorama.Fore.LIGHTBLACK_EX} - Install beatmaps                    {colorama.Fore.LIGHTBLUE_EX}CTRL + ALT + I")
             print(f"{colorama.Fore.LIGHTBLACK_EX} - Show config                         {colorama.Fore.LIGHTBLUE_EX}CTRL + ALT + C")
             print(f"{colorama.Fore.LIGHTBLACK_EX} - Exit                                {colorama.Fore.LIGHTBLUE_EX}CTRL + C")
@@ -88,6 +120,7 @@ class App(object):
             print("Options")
             print(" - Set osu path                        CTRL + ALT + O")
             print(" - Set download path                   CTRL + ALT + D")
+            print(" - Autoload beatmaps                   CTRL + ALT + L")
             print(" - Install beatmaps                    CTRL + ALT + I")
             print(" - Show config                         CTRL + ALT + C")
             print(" - Exit                                CTRL + C")
@@ -101,7 +134,11 @@ class App(object):
         download_path = download.read()
         download.close()
 
-        return osu_path, download_path
+        autoload = open(f"{config_path}\.autoload", "r")
+        autoload_value = autoload.read()
+        autoload.close()
+
+        return osu_path, download_path, autoload_value
 
     def __set_osu_path(self):
         os.system(clear)
@@ -297,6 +334,7 @@ class App(object):
             print(f"{colorama.Fore.YELLOW}Configs")
             print(f"{colorama.Fore.LIGHTBLACK_EX} - Osu path            = {colorama.Fore.LIGHTCYAN_EX}{config[0]}")
             print(f"{colorama.Fore.LIGHTBLACK_EX} - Download path       = {colorama.Fore.LIGHTCYAN_EX}{config[1]}")
+            print(f"{colorama.Fore.LIGHTBLACK_EX} - Autoload            = {colorama.Fore.LIGHTCYAN_EX}{self.autoload}")
             print(f"{colorama.Fore.LIGHTBLACK_EX} - App version         = {colorama.Fore.LIGHTCYAN_EX}{__version__}\n")
             print(f"{colorama.Fore.LIGHTRED_EX}Back to menu {colorama.Fore.WHITE}CTRL + ALT + E")
 
@@ -310,8 +348,26 @@ class App(object):
             print("Configs")
             print(f" - Osu path            = {config[0]}")
             print(f" - Download path       = {config[1]}")
+            print(f" - Autoload            = {self.autoload}")
             print(f" - App version         = {__version__}\n")
             print(f"Back to menu CTRL + ALT + E")
+
+    def __set_autoload(self):
+        if self.autoload == "false":
+            autoload = open(f"{config_path}\.autoload", "w")
+            autoload.write("true")
+            autoload.close()
+
+            self.autoload = "true"
+
+        else:
+            autoload = open(f"{config_path}\.autoload", "w")
+            autoload.write("false")
+            autoload.close()
+
+            self.autoload = "false"
+
+        self.render()
 
     def __exit(self):
         self.render()
